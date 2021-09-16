3b4b2345662d78d6160a8e297a77a670b5528bb0
space pro <85967676+spacepromin@users.noreply.github.com>
2021-09-16 14:51:34 +0300

    comit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,6 @@
  *     * Return: On success 1.
  *      * On error, -1 is returned, and errno is set appropriately.
  *       */
-/**
-int main()
-*{
-*	char ch=0;
-*	_putchar(ch);
-*	return (0);
-}
-*/
 
 int _putchar(char c)
 {
