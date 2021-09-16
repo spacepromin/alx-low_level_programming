05b1105307042b9b9fe53d0f7bb5484ff70839c3
space pro <85967676+spacepromin@users.noreply.github.com>
2021-09-16 14:38:03 +0300

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-//#include "holberton.h"
+#include "holberton.h"
 
 /**
  *  * _putchar - writes the character c to stdout
@@ -8,15 +8,17 @@
  *     * Return: On success 1.
  *      * On error, -1 is returned, and errno is set appropriately.
  *       */
-int main()
-{
-	char ch=0;
-	_putchar(ch);
-	return (0);
-}
 /**
+int main()
+*{
+*	char ch=0;
+*	_putchar(ch);
+*	return (0);
+}
+*/
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-**/
+
